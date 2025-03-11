18301b0d1ee16c3f4d9f61cceacb227e9882830d
Tamilarasan Paranthaman <tamilarasan.paranthaman@syncfusion.com>
2025-03-11 13:23:09 +0530

    WebView issue sample has been added

---
+++ WinUI-WebView-Navigated-Event-Issue/MauiApp1/MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+
+namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void webView_Navigated(object sender, WebNavigatedEventArgs e)
+        {
+            NavigatedLabel.Text = "Navigated event : Called";
+        }
+
+        private void webView_Navigating(object sender, WebNavigatingEventArgs e)
+        {
+            e.Cancel = true;
+            NavigatingLabel.Text = "Navigating event : Called";
+        }
+    }
+}
